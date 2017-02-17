974acae820998af20c7ebaa9d0682cfc3526b9f1
jb <jenniferbeem@gmail.com>
2017-02-17 08:54:30 -0800

    add test function for multiple words/second spec

--- tests/WordSearchTest.php
+++ tests/WordSearchTest.php
@@ -5,12 +5,26 @@
     class SearchTest extends PHPUnit_Framework_TestCase
     {
 
-        function test_occurrence()
+        function test_occurrence_singleWord()
         {
             //Arrange
             $test_Search = new Search;
             $word = "is";
             $phrase = "is";
+
+            //Act
+            $result = $test_Search->occurrence($word, $phrase);
+
+            //Assert
+            $this->assertEquals(1, $result);
+        }
+
+        function test_occurrence_multipleWords()
+        {
+            //Arrange
+            $test_Search = new Search;
+            $word = "is";
+            $phrase = "substr_count is a pretty cool function";
 
             //Act
             $result = $test_Search->occurrence($word, $phrase);
