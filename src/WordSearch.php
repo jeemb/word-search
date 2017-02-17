f1c86294047f29caa105ea961bd1b9a3be64b45d
jb <jenniferbeem@gmail.com>
2017-02-17 09:21:49 -0800

    amend occurrence function to only search for full word matches

--- src/WordSearch.php
+++ src/WordSearch.php
@@ -4,8 +4,17 @@
         function occurrence($word, $phrase)
         {
         $result;
-        $number = substr_count($phrase, $word);
-        $result = $number;
+        $matching_words = array();
+        $full_words = explode(" ", $phrase);
+        foreach ($full_words as $singleword) {
+            if ($singleword === $word) {
+                array_push($matching_words, $word);
+            }
+        }
+        $count_array = array_count_values($matching_words);
+        $number_of_matches = $count_array[$word];
+        // $number = substr_count($phrase, $word);
+        $result = $number_of_matches;
         return $result;
 
     }
